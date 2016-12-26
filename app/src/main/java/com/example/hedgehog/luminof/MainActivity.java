15caa64db2b8ea65f7d036b1fdf356ed61c36ea9
hedgehog <research162@gmail.com>
2016-12-27 00:48:35 +0200

    side menu

--- app/src/main/java/com/example/hedgehog/luminof/MainActivity.java
+++ app/src/main/java/com/example/hedgehog/luminof/MainActivity.java
@@ -1,16 +1,99 @@
 package com.example.hedgehog.luminof;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    ArrayList<Item> menuTitles = new ArrayList<>();{
+        menuTitles.add(Item.CATALOG);
+        menuTitles.add(Item.PAYMENT);
+        menuTitles.add(Item.CONTACTS);
+        menuTitles.add(Item.FEEDBACK);
+    }
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new MenuAdapter(this, menuTitles));
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,  R.string.app_name, R.string.app_name){
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public enum Item {
+        CATALOG(R.string.catalog), PAYMENT(R.string.payment), CONTACTS(R.string.contacts), FEEDBACK(R.string.feedback);
+        private final int resourse;
+        Item (int resourse) {
+            this.resourse = resourse;
+        }
+        public int getResourse(){
+            return resourse;
+        }
+    }
+
+
 }
+
---
+++ app/src/main/java/com/example/hedgehog/luminof/MenuAdapter.java
@@ -0,0 +1,71 @@
+package com.example.hedgehog.luminof;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hedgehog on 26.12.2016.
+ */
+
+public class MenuAdapter extends BaseAdapter {
+
+    private ArrayList<MainActivity.Item> arrayList;
+    private LayoutInflater inflater;
+    private Context context;
+
+    public MenuAdapter(Context context, ArrayList<MainActivity.Item> arrayList){
+        this.arrayList = arrayList;
+        this.context = context;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public MainActivity.Item getItem(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View rootView = convertView;
+        ViewHolder vh;
+        if (convertView == null) {
+            rootView = inflater.inflate(R.layout.menu_item_layout, parent, false);
+            vh = new ViewHolder(rootView);
+            rootView.setTag(vh);
+        } else {
+            vh = (ViewHolder) rootView.getTag();
+        }
+
+        vh.tvName.setText(arrayList.get(position).getResourse());
+        return rootView;
+    }
+
+    private class ViewHolder{
+        View rootView;
+        TextView tvName;
+
+        ViewHolder(View itemView) {
+            rootView = itemView;
+            tvName = (TextView) rootView.findViewById(R.id.tvName);
+
+
+        }
+    }
+}
