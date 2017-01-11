8a250976be7ffb4b6c260beb272a367a4fc2da73
hedgehog <research162@gmail.com>
2017-01-11 04:51:36 +0200

    Catalog fragment

--- app/src/main/java/com/example/hedgehog/luminof/fragment/CatalogFragment.java
+++ app/src/main/java/com/example/hedgehog/luminof/fragment/CatalogFragment.java
@@ -3,9 +3,15 @@ package com.example.hedgehog.luminof.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.example.hedgehog.luminof.R;
 
 
@@ -20,8 +26,18 @@ public class CatalogFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_catalog, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_catalog, container, false);
+
+       /* TextView tvLuminofor = (TextView) rootView.findViewById(R.id.tvLuminofor);
+        Spannable spans = new SpannableString(getContext().getResources().getString(R.string.luminofor));
+        spans.setSpan(new ForegroundColorSpan(ContextCompat.getColor(getContext(), R.color.colorProductTitle)), 0, 9, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tvLuminofor.setText(spans);*/
+
+       /* TextView tvFluorescent = (TextView) rootView.findViewById(R.id.tvFluorescent);
+        spans = new SpannableString(getContext().getResources().getString(R.string.fluorescent));
+        spans.setSpan(new ForegroundColorSpan(ContextCompat.getColor(getContext(), R.color.colorProductTitle)), 0, 11, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tvFluorescent.setText(spans);*/
+        return rootView;
     }
 
 }
